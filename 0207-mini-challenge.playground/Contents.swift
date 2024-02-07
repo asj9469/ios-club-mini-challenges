77f31e4b8e7d48d48e2a5ce416738ef3e8bbed7c
asj9469 <ghs051965@gmail.com>
2024-02-07 18:27:53 -0500

    updated 0207 mini challenge

--- 0207-mini-challenge.playground/Contents.swift
+++ 0207-mini-challenge.playground/Contents.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+
+//MINI CHALLENGE 1
+
 func sayHello() {
     print("Hello!")
 }
@@ -11,7 +14,53 @@ var greet: (()->()) = sayHello
 greet
 greet()
 
-let sayHelloClosure = { print("Hello there!") }
+let sayHelloClosure = {
+    print("Hello there!")
+    return "hi"
+}
 
+// if you don't add the (), it returns the type of the function
+// if you add the (), you're calling for the output for it -> actually prints out stuff & returns stuff
 sayHelloClosure
 sayHelloClosure()
+
+
+//MINI CHALLENGE 2
+
+let sayHelloClosureWithParameter = { (str: String)->(Void) in
+    print("Hello there, \(str)")
+}
+
+sayHelloClosureWithParameter("Leo")
+
+let sayhelloClosureWithParameterAndRet = {(name: String) -> (String) in
+    
+    return "Hello there, \(name)"
+}
+
+sayhelloClosureWithParameterAndRet("Alec")
+
+func mySeatingArrangement (neighbors: (([String])->())){
+    
+    neighbors(["Leo", "Alec"])
+}
+
+mySeatingArrangement(neighbors: { (names: [String]) -> () in
+    //using map
+    print("I sit next to", names.map{ $0 }.joined(separator: " and "))
+    
+    //OR use a for loop
+    var output: String = "I sit next to "
+    for name in names{
+        output.append(name)
+        if (name != names[names.count - 1]){
+            output.append(" and ")
+        }else{
+            output.append(".")
+        }
+    }
+    
+    print(output)
+})
+
+//
